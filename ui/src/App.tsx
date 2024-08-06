3b0bf17019f8a5ae98321d8eb030506c3dbc4ed2
arshkhatwani <arsh.khatwani@gmail.com>
2024-08-07 02:00:08 +0530

    adds logic to create meeting

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,28 +2,42 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+    const [meetingId, setMeetingId] = useState<string>("");
 
-  useEffect(() => {
-    const socket = new WebSocket("ws://localhost:8080");
-    const pc = new RTCPeerConnection();
-    setSocket(socket)
-    setPC(pc);
-  }, []);
+    useEffect(() => {
+        const socket = new WebSocket("ws://localhost:8080");
+        const pc = new RTCPeerConnection();
+        setSocket(socket);
+        setPC(pc);
+    }, []);
 
-  const createMeeting = async () => {
-    if(!socket || !pc) return;
-    
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
+    const createMeeting = async () => {
+        if (!socket || !pc) return;
 
-    socket.send(JSON.stringify({type: 'createOffer', sdp: offer}));
-  }
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+
+            if (message.type === "offerCreated") {
+                setMeetingId(message.meetingId);
+            } else if (message.type === "createAnswer") {
+                console.log(message);
+            } else if (message.type === "iceCandidate") {
+                console.log("ice candidate");
+            }
+        };
+
+        socket.send(JSON.stringify({ type: "createOffer", sdp: offer }));
+    };
 
     return (
         <>
-            <h1>Hello world</h1>
+            <button onClick={createMeeting}>Create Meeting</button>
+            {meetingId && <p>Your meeting id is {meetingId}</p>}
         </>
     );
 }
