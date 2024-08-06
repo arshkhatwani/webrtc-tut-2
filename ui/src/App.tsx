d1a6b9a62170231ad2e9e8c198d3bef795e6ae43
arshkhatwani <arsh.khatwani@gmail.com>
2024-08-06 23:55:55 +0530

    add createMeeting function in app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    const pc = new RTCPeerConnection();
+    setSocket(socket)
+    setPC(pc);
+  }, []);
+
+  const createMeeting = async () => {
+    if(!socket || !pc) return;
+    
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+
+    socket.send(JSON.stringify({type: 'createOffer', sdp: offer}));
+  }
+
     return (
         <>
             <h1>Hello world</h1>
