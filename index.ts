b865a1b14214bea2b360b5c024b70ccd39cd95ac
arshkhatwani <arsh.khatwani@gmail.com>
2024-08-06 23:56:56 +0530

    adds message types with approach

--- index.ts
+++ index.ts
@@ -7,7 +7,21 @@ const server = new ws.Server({ port: PORT }, () => {
 });
 
 server.on("connection", (socket) => {
-    socket.on("message", (data) => {
-        console.log(data.toString());
+    socket.on("message", (data: any) => {
+      const message = JSON.parse(data);
+
+      if(message.type === 'createOffer') {
+        console.log('Offer received');
+        // store it in a hashmap with a random key and its socket and sdp as value
+      }
+      else if(message.type === 'createAnswer') {
+        console.log('Answer recieved')
+        // fetch the value from the hashmap using that random key and share the answer with that sender
+        // also do something to check if the sender and receiver are already matched or not
+      }
+      else if(message.type === 'iceCandidate') {
+        console.log('Ice candidate received');
+        // fetch the value from hashmap verify whether the socket is receiver or sender and share the ice candidates accordingly
+      }
     });
 });
